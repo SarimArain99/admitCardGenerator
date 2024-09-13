605f161f8de829cc2058e182100e5208fc1ce0e2
Muhammad Sarim <sarimarain99@gmail.com>
2024-09-13 10:09:51 +0500

    Modified

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 "use strict";
+var _a;
 const form = document.getElementById('admitForm');
 const admitCard = document.getElementById('admitCard');
 const printButton = document.getElementById('printButton');
-let rollNumber = parseInt(localStorage.getItem('rollNumber') || '', 10);
+let rollNumber = parseInt((_a = localStorage.getItem('rollNumber')) !== null && _a !== void 0 ? _a : '1', 10);
+if (isNaN(rollNumber)) {
+    rollNumber = 1;
+}
 form.addEventListener('submit', (event) => {
     event.preventDefault();
-    // Get user inputs
     const fullName = document.getElementById('fullName').value;
     const fathersName = document.getElementById('fathersName').value;
     const dob = document.getElementById('dob').value;
     const profilePictureInput = document.getElementById('profilePicture');
-    // Update Admit Card Details
     (document.getElementById('admitFullName')).textContent = `Full Name: ${fullName}`;
     (document.getElementById('admitFathersName')).textContent = `Father's Name: ${fathersName}`;
     (document.getElementById('admitDOB')).textContent = `Date of Birth: ${dob}`;
     (document.getElementById('admitRollNumber')).textContent = `Roll Number: ${rollNumber.toString().padStart(3, '0')}`;
-    // Handle Profile Picture if provided
     const profileImg = document.getElementById('profileImg');
     if (profilePictureInput.files && profilePictureInput.files[0]) {
         const reader = new FileReader();
